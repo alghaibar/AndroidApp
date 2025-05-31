f7c271c80e0fc98974faaf4daa67265520cd21a9
alghaibar <ehab.cs.2016@gmail.com>
2025-05-31 23:55:53 +0300

    first regenarate traveling app

---
+++ traveling_app/lib/widgets/category_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../screens/category_trips_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const CategoryItem(this.id, this.title, this.imageUrl, {super.key});
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).pushNamed(
+      CategoryTripsScreen.screenRoute,
+      arguments: {'id': id, 'title': title},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.network(imageUrl, height: 250, fit: BoxFit.cover),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.4),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Text(title, style: Theme.of(context).textTheme.titleLarge),
+          ),
+        ],
+      ),
+    );
+  }
+}
